5eb0d81e032afc00b58b346240f523e8ca9534b9
kalaivananarjunan12-cpu <kalaivananarjunan12@gmail.com>
2026-08-20 21:05:51 +0530

    Add files via upload

---
+++ reverse_number.py
@@ -0,0 +1,12 @@
+# Reverse Number Program
+
+number = int(input("Enter a number: "))
+
+reverse = 0
+
+while number > 0:
+    digit = number % 10
+    reverse = reverse * 10 + digit
+    number = number // 10
+
+print("Reversed number:", reverse)
